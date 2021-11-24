553b01c1ed9335a36142d934e15ccbaa509c5185
Ryan Wenger <ryanwenger@umail.ucsb.edu>
2021-11-24 14:00:54 -0800

    Starter files

---
+++ mipsmulti.v
@@ -0,0 +1,54 @@
+//-------------------------------------------------------
+// Multicycle MIPS processor
+//------------------------------------------------
+
+module mips(input        clk, reset,
+            output [31:0] adr, writedata,
+            output        memwrite,
+            input [31:0] readdata);
+
+  wire        zero, pcen, irwrite, regwrite,
+               alusrca, iord, memtoreg, regdst;
+  wire [1:0]  alusrcb, pcsrc;
+  wire [2:0]  alucontrol;
+  wire [5:0]  op, funct;
+
+  controller c(clk, reset, op, funct, zero,
+               pcen, memwrite, irwrite, regwrite,
+               alusrca, iord, memtoreg, regdst, 
+               alusrcb, pcsrc, alucontrol);
+  datapath dp(clk, reset, 
+              pcen, irwrite, regwrite,
+              alusrca, iord, memtoreg, regdst,
+              alusrcb, pcsrc, alucontrol,
+              op, funct, zero,
+              adr, writedata, readdata);
+endmodule
+
+// Todo: Implement controller module
+module controller(input       clk, reset,
+                  input [5:0] op, funct,
+                  input       zero,
+                  output       pcen, memwrite, irwrite, regwrite,
+                  output       alusrca, iord, memtoreg, regdst,
+                  output [1:0] alusrcb, pcsrc,
+                  output [2:0] alucontrol);
+
+// **PUT YOUR CODE HERE**
+ 
+endmodule
+
+// Todo: Implement datapath
+module datapath(input        clk, reset,
+                input        pcen, irwrite, regwrite,
+                input        alusrca, iord, memtoreg, regdst,
+                input [1:0]  alusrcb, pcsrc, 
+                input [2:0]  alucontrol,
+                output [5:0]  op, funct,
+                output        zero,
+                output [31:0] adr, writedata, 
+                input [31:0] readdata);
+
+// **PUT YOUR CODE HERE** 
+
+endmodule
